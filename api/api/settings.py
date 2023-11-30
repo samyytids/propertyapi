537b36cbd5660f6f5117c72f2634866fb1f94148
samyytids <sta.james@outlook.com>
2023-11-30 18:21:38 +0000

    FOR FUCK SAKE COME ON seriously...

--- api/api/settings.py
+++ api/api/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "NAME": os.environ.get("SQL_DB", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
